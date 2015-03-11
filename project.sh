448f5780151ef13779607c45456c3eda75fbd15f
root <root@fuel.domain.tld>
2015-03-11 15:39:34 +0000

    compute auto_scaling ready

--- project.sh
+++ project.sh
@@ -1,28 +1,5 @@
 #!/bin/bash
 
-# Defaults 1: Ceph and compute nedes have the same HW configuration.
-# Defaults 2: Nodes Prebootstrapped and turned off, waiting for WOL packet.
-# Defaults 3: Nodes are renamed, and have no spaces in names. This is URGENT!!!
-# Defaults 4: to be added...
-
-# Steps:
-# Step 1: Collect infromation about the nodes.
-# Step 2: Create pool of hash for storage and compute nodes.
-# Step 3: Send magick packet (WOL) for actual node.
-# Step 4: Monitor that the node is online and ready to deploy.
-# Step 5: Add nodes to cluster ID.
-# Step 6: Configure the node. Patch the YAML.(mac, ip)
-# Step 7: Upload configuration back to FUEL.
-# Step 8: Deploy compute/ceph node.
-
-# Commands:
-# boot compute: bootstrap one compute node
-# boot storage: bootstrap one storage node
-# deploy compute: deploy one compute node
-# deploy storage: deploy one storage node
-# remove node_name: remove node-id from fuel database, U still need to remove tails from cobbler;
-# ren node_name: 
-
 confdir="config"
 . "$confdir/config"
 . "$confdir/functions"
@@ -48,15 +25,12 @@ command_seq=$COMMANDS_SEQUENCE
 if [ "$1" == "list" ]; then
   shift 1
   list_nodes
-elif [ "$1" == "update_creds" ]; then
-  shift 1
-  update_credentials
-elif [ "$1" == "node" ]; then
+elif [ "$1" == "info" ]; then
   shift 1
   node_data "$@"
 elif [ "$1" == "boot" ]; then
   shift 1
-  get_node_id "$1"
+  wol_and_check "$1"
 elif [ "$1" == "deploy" ]; then
   shift 2
   deploy $container $@
@@ -69,6 +43,10 @@ elif [ "$1" == "remove" ]; then
 elif [ "$1" == "rename" ]; then
   shift 2
   rename $container "$@"
+elif [ "$1" == "update_creds" ]; then
+  shift 1
+  update_credentials
+
 else
   echo "Invalid selection."
   show_usage
